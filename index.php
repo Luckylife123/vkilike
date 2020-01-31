b92a94278c97b3120d4653c0d62caa4ed349fe35
Viktor Dmytruk <bitbok@i.ua>
2020-01-31 14:21:26 +0200

    Catch error curl

--- index.php
+++ index.php
@@ -3,43 +3,26 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
-	<script src="js/jquery-3.3.1.min.js"></script>
-	<script type="text/javascript" src="http://vkontakte.ru/js/api/openapi.js"></script>
 
 </head>
 <body>
 <?php
-require_once __DIR__.'/vendor/autoload.php';
-
-session_start();
-if(!$_SESSION['access_token']){
-    $oauth = new VK\OAuth\VKOAuth();
-    $client_id = 7302376;
-    $redirect_uri = 'http://vk-posts.tmweb.ru/';
-    $display = VK\OAuth\VKOAuthDisplay::PAGE;
-    $scope = array(VK\OAuth\Scopes\VKOAuthUserScope::WALL, VK\OAuth\Scopes\VKOAuthUserScope::GROUPS);
-    $state = '';
-    $browser_url = $oauth->getAuthorizeUrl(VK\OAuth\VKOAuthResponseType::CODE, $client_id, $redirect_uri, $display, $scope, $state);
-    if(!$_GET['code']){
-        echo  '<a href="'.$browser_url.'"/>Url auth</a>';
-    }
-    else{
-        $code = $_GET['code'];
-        $client_secret = 'XDQY2tbz2dLSWigCI4FA';
-        $response = $oauth->getAccessToken($client_id, $client_secret, $redirect_uri, $code);
-        $access_token = $response['access_token'];
-        if($access_token){
-            $_SESSION['access_token'] = $access_token;
-            echo $access_token;
-            echo $_SESSION['access_token'];
-        }
-    }
-}else{
-    echo $_SESSION['access_token'];
+require_once __DIR__ . '/vendor/autoload.php';
+$oauth = new VK\OAuth\VKOAuth();
+$client_id = 7302376;
+$redirect_uri = 'http://vk-posts.tmweb.ru/';
+$display = VK\OAuth\VKOAuthDisplay::PAGE;
+$scope = [VK\OAuth\Scopes\VKOAuthUserScope::WALL, VK\OAuth\Scopes\VKOAuthUserScope::GROUPS];
+$state = '';
+$browser_url = $oauth->getAuthorizeUrl(VK\OAuth\VKOAuthResponseType::CODE, $client_id, $redirect_uri, $display, $scope, $state);
+echo '<a href="' . $browser_url . '"/>Url auth</a>';
+if($_GET){
+    $code = $_GET['code'];
+    $client_secret = 'XDQY2tbz2dLSWigCI4FA';
+    $response = $oauth->getAccessToken($client_id, $client_secret, $redirect_uri, $code);
+    $access_token = $response['access_token'];
 }
-
 ?>
-
 
 
 </body>
