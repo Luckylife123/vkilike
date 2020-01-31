e3d49ef6a8717fc9713f5fdb5bb3103d74c3ffd3
Viktor Dmytruk <bitbok@i.ua>
2020-01-31 14:16:00 +0200

    Catch error curl

--- index.php
+++ index.php
@@ -11,24 +11,30 @@
 <?php
 require_once __DIR__.'/vendor/autoload.php';
 
-
-$vk = new  VK\Client\VKApiClient();
-
-$oauth = new VK\OAuth\VKOAuth();
-$client_id = 7302376;
-$redirect_uri = 'http://vk-posts.tmweb.ru/';
-$display = VK\OAuth\VKOAuthDisplay::PAGE;
-$scope = array(VK\OAuth\Scopes\VKOAuthUserScope::WALL, VK\OAuth\Scopes\VKOAuthUserScope::GROUPS);
-$state = '';
-$browser_url = $oauth->getAuthorizeUrl(VK\OAuth\VKOAuthResponseType::CODE, $client_id, $redirect_uri, $display, $scope, $state);
-echo  '<a href="'.$browser_url.'"/>Url auth</a>';
-$code = $_GET['code'];
-if($code){
-    $client_secret = 'XDQY2tbz2dLSWigCI4FA';
-    $response = $oauth->getAccessToken($client_id, $client_secret, $redirect_uri, $code);
-    print_r($response);
-    $access_token = $response['access_token'];
-    die($access_token);
+session_start();
+if(!$_SESSION['access_token']){
+    $oauth = new VK\OAuth\VKOAuth();
+    $client_id = 7302376;
+    $redirect_uri = 'http://vk-posts.tmweb.ru/';
+    $display = VK\OAuth\VKOAuthDisplay::PAGE;
+    $scope = array(VK\OAuth\Scopes\VKOAuthUserScope::WALL, VK\OAuth\Scopes\VKOAuthUserScope::GROUPS);
+    $state = '';
+    $browser_url = $oauth->getAuthorizeUrl(VK\OAuth\VKOAuthResponseType::CODE, $client_id, $redirect_uri, $display, $scope, $state);
+    if(!$_GET['code']){
+        echo  '<a href="'.$browser_url.'"/>Url auth</a>';
+    }
+    else{
+        $code = $_GET['code'];
+        $client_secret = 'XDQY2tbz2dLSWigCI4FA';
+        $response = $oauth->getAccessToken($client_id, $client_secret, $redirect_uri, $code);
+        $access_token = $response['access_token'];
+        if($access_token){
+            $_SESSION['access_token'] = $access_token;
+            echo $_SESSION['access_token'];
+        }
+    }
+}else{
+    echo $_SESSION['access_token'];
 }
 
 ?>
