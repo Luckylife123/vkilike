5a9fc2c531f28f4249236a94db312b666887d3d9
Viktor Dmytruk <bitbok@i.ua>
2020-01-31 15:36:40 +0200

    ADd post

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 
 $oauth = new VK\OAuth\VKOAuth();
-$client_id = 7302376;
+$client_id = 7302576;
 $redirect_uri = 'http://vk-posts.tmweb.ru/';
 $display = VK\OAuth\VKOAuthDisplay::PAGE;
 $scope = [VK\OAuth\Scopes\VKOAuthUserScope::WALL, VK\OAuth\Scopes\VKOAuthUserScope::GROUPS, VK\OAuth\Scopes\VKOAuthUserScope::OFFLINE];
@@ -22,7 +22,7 @@ $browser_url = $oauth->getAuthorizeUrl(VK\OAuth\VKOAuthResponseType::CODE, $clie
 echo '<a href="' . $browser_url . '"/>Url auth</a>';
 if($_GET['code']){
     $code = $_GET['code'];
-    $client_secret = 'XDQY2tbz2dLSWigCI4FA';
+    $client_secret = 'c0XrAfeDXiPVZSqN82ay';
     $response = $oauth->getAccessToken($client_id, $client_secret, $redirect_uri, $code);
     $access_token = $response['access_token'];
     print_r($response);
