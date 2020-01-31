a4ca19a62ac9292b4d9ca70d6c13359838abc58b
Viktor Dmytruk <bitbok@i.ua>
2020-01-31 14:24:34 +0200

    Delete session

--- index.php
+++ index.php
@@ -8,6 +8,27 @@
 <body>
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
+
+session_start();
+
+// Удаляем все переменные сессии.
+$_SESSION = array();
+
+// Если требуется уничтожить сессию, также необходимо удалить сессионные cookie.
+// Замечание: Это уничтожит сессию, а не только данные сессии!
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// Наконец, уничтожаем сессию.
+session_destroy();
+
+
+
 $oauth = new VK\OAuth\VKOAuth();
 $client_id = 7302376;
 $redirect_uri = 'http://vk-posts.tmweb.ru/';
