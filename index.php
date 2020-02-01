b21368ac5e3b1459360f287e64cf377d7c4ded8a
Viktor Dmytruk <bitbok@i.ua>
2020-02-01 14:20:32 +0200

    test post in group

--- index.php
+++ index.php
@@ -6,31 +6,7 @@
 
 </head>
 <body>
-<?php
-$group_id = "-176950270";
-$token = '55793f142a35b289949f684006a1972e9948a708570b01dbcb5c090d179adb272a2c86186f85a8e6ececd';
-$text = 'test';
+<a href=https://oauth.vk.com/authorize?client_id=1&7064288display=page&redirect_uri=http://vk-posts.tmweb.ru&scope=wall&response_type=token&v=5.103&state=123456'>link</a>
 
-
-$url = 'https://api.vk.com/method/wall.post?';
-$ch = curl_init();
-curl_setopt_array($ch, array(
-	CURLOPT_HEADER => TRUE,
-	CURL_POST => TRUE,
-	CURLOPT_RETURNTRANSFER => TRUE,
-	CURLOPT_SSL_VERIFYPEER => FALSE,
-	CURLOPT_SSL_VERIFYHOST => FALSE,
-	CURLOPT_POSTFIELDS     => array(
-		"owner_id"     => $group_id,
-		"from_group"   => 1,
-		"Message"      => $text,
-		"access_token" => $token
-	),
-	CURLOPT_URL => $url,
-));
-$query = curl_exec($ch);
-curl_close($ch);
-print_r($query);
-?>
 </body>
 </html>
