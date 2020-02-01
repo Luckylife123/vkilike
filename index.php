cd4c661432294228dd575995852c75d1c024754e
Viktor Dmytruk <bitbok@i.ua>
2020-02-01 14:20:52 +0200

    test post in group

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 </head>
 <body>
-<a href=https://oauth.vk.com/authorize?client_id=1&7064288display=page&redirect_uri=http://vk-posts.tmweb.ru&scope=wall&response_type=token&v=5.103&state=123456'>link</a>
+<a href=https://oauth.vk.com/authorize?client_id=7064288display=page&redirect_uri=http://vk-posts.tmweb.ru&scope=wall&response_type=token&v=5.103&state=123456'>link</a>
 
 </body>
 </html>
