49be096496d7fd481b7da6c956ad3ce1af5ec188
Viktor Dmytruk <bitbok@i.ua>
2020-02-01 15:01:11 +0200

    test post in group

--- index.php
+++ index.php
@@ -8,8 +8,9 @@
 <body>
 <?php
 $access_token = 'd64bc24c383c5ea517ec9c74b9ba6f94c46ade0c3554ddfb6f532bf6159f4aaf4f462c1585edcf5782c9a';
-echo "test";
+
 $vk = new VK\Client\VKApiClient();
+echo "test";
 $respons = $vk->wall()->post($access_token,array(
 		'owner_id' => '27162548',
 		'message'  => 'test'
