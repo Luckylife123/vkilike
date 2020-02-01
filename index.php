efa6f1a673f6070c0557e0309184f6d023506243
Viktor Dmytruk <bitbok@i.ua>
2020-02-01 13:57:40 +0200

    test post in group

--- index.php
+++ index.php
@@ -7,45 +7,34 @@
 </head>
 <body>
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
+$group_id = "-176950270";
+$token = '55793f142a35b289949f684006a1972e9948a708570b01dbcb5c090d179adb272a2c86186f85a8e6ececd';
+$text = 'test';
 
-
-
-
-$oauth = new VK\OAuth\VKOAuth();
-$client_id = 7302576;
-$redirect_uri = 'http://vk-posts.tmweb.ru';
-$display = VK\OAuth\VKOAuthDisplay::PAGE;
-$scope = [VK\OAuth\Scopes\VKOAuthUserScope::WALL, VK\OAuth\Scopes\VKOAuthUserScope::GROUPS, VK\OAuth\Scopes\VKOAuthUserScope::OFFLINE];
-$state = '';
-//Authorization Code Flow
-//$browser_url = $oauth->getAuthorizeUrl(VK\OAuth\VKOAuthResponseType::CODE, $client_id, $redirect_uri, $display, $scope, $state);
-//echo '<a href="' . $browser_url . '"/>Url auth</a>';
-//if($_GET['code']){
-//    $code = $_GET['code'];
-//    $client_secret = 'c0XrAfeDXiPVZSqN82ay';
-//    $response = $oauth->getAccessToken($client_id, $client_secret, $redirect_uri, $code);
-//    $access_token = $response['access_token'];
-//}
-
-
-//Implicit flow
-$browser_url = $oauth->getAuthorizeUrl(VK\OAuth\VKOAuthResponseType::TOKEN, $client_id, $redirect_uri, $display, $scope, $state, null, false);
-echo '<a href="' . $browser_url . '"/>Url auth</a>';
-
-    $access_token = '55793f142a35b289949f684006a1972e9948a708570b01dbcb5c090d179adb272a2c86186f85a8e6ececd';
-
-if($access_token){
-	echo $access_token;
-    $vk = new VK\Client\VKApiClient();
-    $response = $vk->wall()->post($access_token, array(
-        'owner_id' => '27162548',
-        'message' => 'test',
-    ));
-    echo "test";
+$url = printf('https://api.vk.com/method/wall.post?');
+$ch = curl_init();
+curl_setopt_array($ch, array(
+	CURL_POST => TRUE,
+	CURLOPT_RETURNTRANSFER => TRUE,
+	CURLOPT_SSL_VERIFYPEER => FALSE,
+	CURLOPT_SSL_VERIFYHOST => FALSE,
+	CURLOPT_POSTFIELDS     => array(
+		"owner_id"     => $group_id,
+		"from_group"   => 1,
+		"Message"      => $text,
+		"access_token" => $token
+	),
+	CURLOPT_URL => $url,
+));
+$query = curl_exec($ch);
+curl_close($ch);
+if(!$query){
+	printf('error');
+}
+else{
+	printf("ok");
 }
 ?>
-
 
 </body>
 </html>
