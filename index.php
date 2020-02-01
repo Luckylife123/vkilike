eb9ffb0c28472e47ddf152a9e81560132ce3517d
Viktor Dmytruk <bitbok@i.ua>
2020-02-01 15:07:42 +0200

    test post in group

--- index.php
+++ index.php
@@ -7,16 +7,15 @@
 </head>
 <body>
 <?php
-require_once ('vendor/vkcom/vk-php-sdk/src');
 $access_token = 'd64bc24c383c5ea517ec9c74b9ba6f94c46ade0c3554ddfb6f532bf6159f4aaf4f462c1585edcf5782c9a';
 
 $vk = new VK\Client\VKApiClient();
-
-$respons = $vk->wall()->post($access_token,array(
-		'owner_id' => '27162548',
-		'message'  => 'test'
-	));
-print_r($respons);
+echo "mytest";
+$response = $vk->wall()->post($access_token,array(
+    'owner_id' => '27162548',
+    'message'  => 'test'
+));
+print_r($response);
 echo 'tset';
 ?>
 </body>
