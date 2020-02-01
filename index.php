0ea5a5929b07c780f961dce216771e7b857861c9
Viktor Dmytruk <bitbok@i.ua>
2020-02-01 15:13:44 +0200

    Vk api

--- index.php
+++ index.php
@@ -9,8 +9,10 @@
 <?php
 
 echo "test1";
+$oauth = new VK\OAuth\VKOAuth();
+echo "test2";
 $vk = new VK\Client\VKApiClient();
-echo 'test2';
+echo 'test3';
 $access_token = 'd64bc24c383c5ea517ec9c74b9ba6f94c46ade0c3554ddfb6f532bf6159f4aaf4f462c1585edcf5782c9a';
 $response = $vk->wall()->post($access_token,array(
     'owner_id' => '27162548',
