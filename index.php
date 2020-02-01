a1fd6b37c33d6099435358bf497d8e8e35ca0c4f
Viktor Dmytruk <bitbok@i.ua>
2020-02-01 17:43:25 +0200

    get post

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 <?php
 //get access token https://oauth.vk.com/authorize?client_id=7302576&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=wall,offline,groups&response_type=token&v=5.103
 $access_token = 'd64bc24c383c5ea517ec9c74b9ba6f94c46ade0c3554ddfb6f532bf6159f4aaf4f462c1585edcf5782c9a';
+include('posting.php');
 $posting = new Posting($access_token);
 $posts = $posting->getPosts('123302199');
 $getText = $posting->addToPosting($posts);
