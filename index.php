9f215d048acf43decbc4f413affca897de1bb88b
Viktor Dmytruk <bitbok@i.ua>
2020-02-01 14:54:20 +0200

    test post in group

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 </head>
 <body>
 <?php
-$access_token = 'd27d9dbb141037e4ff9053415240dc3e96a2cc2c28f95b1e865677634391800bbb4c76376e543de7ef44a';
+$access_token = 'b78a3c763183ee74dc4b53e190236f9d80f3c4e12e1cc52449fdd980fca726adafd566241f0411033a748';
 $vk = new VK\Client\VKApiClient();
 $respons = $vk->wall()->post($access_token,array(
 		'owner_id' => '27162548',
