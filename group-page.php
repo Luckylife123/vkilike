70cf93319d1fa640d0bb974d16efce7e670faaf9
Viktor Dmytruk <bitbok@i.ua>
2020-02-02 20:23:06 +0200

    vk-group-id

--- group-page.php
+++ group-page.php
@@ -1,12 +1,12 @@
 <?php include_once ('header.php')?>
 <?php include_once('connect-to-db.php'); ?>
 <?php
-$sql = "SELECT id, post_text, post_imgages FROM Posts WHERE vk_group_id = ".$_GET['vk_group_id'];
+$sql = "SELECT id, post_text, post_imgages FROM Posts WHERE vk_group_id = '" . $_GET['vk-group-id'] ."'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()):?>
-        <div style="padding: 10px 0">
+        <div style="padding: 10px 0;border: 1px solid #000;">
             <div>
                 <?php echo $row['post_text'];?>
             </div>
