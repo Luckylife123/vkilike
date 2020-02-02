aecbd58ea3c773d598899e8fba1e34ba97982ce2
Viktor Dmytruk <bitbok@i.ua>
2020-02-02 20:12:10 +0200

    Created db

--- connect-to-db.php
+++ connect-to-db.php
@@ -1,7 +1,7 @@
 <?php
 $dbhost = "localhost";
 $dbuser = "cr27008";
-$dbpass = "ayLU2t1V";
+$dbpass = "ayLU2t1Ve";
 $db = "cr27008_vposts";
 $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
 ?>
