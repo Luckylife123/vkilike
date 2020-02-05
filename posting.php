0a37ee63616ca7b0b3e1aea1cd66fcc50fa1c53c
Viktor Dmytruk <bitbok@i.ua>
2020-02-05 17:00:35 +0200

    Added post upload images

--- posting.php
+++ posting.php
@@ -41,7 +41,7 @@ class Posting
         $attachments_codes = [];
         foreach ($attachments as $attachment){
             $result = $this->vkApiClient->getRequest()->upload($upload_url,'photo',$attachment);
-            print_r($result->photo);
+            print_r(json_decode($result));
             die($attachment);
 
             //            array_push($attachments_codes, json_decode($this->curlHttpClient->post($upload_url,[
