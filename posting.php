937f2ea247738164d1876c7e78bdcff36467a013
Viktor Dmytruk <bitbok@i.ua>
2020-02-05 18:55:08 +0200

    Added post upload images

--- posting.php
+++ posting.php
@@ -66,6 +66,7 @@ class Posting
     public function uploadToVk($attachments,$upload_url){
         $attachments_codes = [];
         foreach ($attachments as $attachment){
+            echo $attachment;
             print_r(realpath('http://vk-posts.tmweb.ru/'.$attachment));
             die(realpath('http://vk-posts.tmweb.ru/'.$attachment));
             array_push($attachments_codes, $this->vkApiClient->getRequest()->upload($upload_url,'photo',realpath('http://vk-posts.tmweb.ru/'.$attachment)));
