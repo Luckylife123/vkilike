5862a1a96692dc1cdcea7b3dd2fc68a73135bd21
Viktor Dmytruk <bitbok@i.ua>
2020-02-05 16:36:01 +0200

    Added post upload images

--- posting.php
+++ posting.php
@@ -52,6 +52,7 @@ class Posting
         $result = $this->vkApiClient->photos()->getWallUploadServer($this->access_token,[
             'group_id' => $groupId
         ]);
+        die($result);
         $upload_url = $result['upload_uri'];
         return $upload_url;
     }
