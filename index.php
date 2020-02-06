89adfa99e2e3b321042adda5aa1f2bbbcff26842
Viktor Dmytruk <bitbok@i.ua>
2020-02-06 19:12:01 +0200

    test date

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
 	<input type="text" name="start_time_for_post" required>
 
 	<label for="period_posts">Періодичність постів в (хв)</label>
-	<input type="text" name="period_posts" value="<?php echo date('y-m-d h:m')?>" required>
+	<input type="text" name="period_posts" value="<?php echo date('y-m-d h:m', 'Euro/Kiev')?>" required>
 
 	<button type="submit">Добавити пости</button>
 </form>
