d8f053abda6bf16ba76f54cf744fc2953e20d5f4
Viktor Dmytruk <bitbok@i.ua>
2020-02-06 18:49:16 +0200

    test date

---
+++ post-to-vk.php
@@ -0,0 +1,90 @@
+<?php include_once('connect-to-db.php'); ?>
+<?php include_once('posting.php'); ?>
+<?php
+deleteSavedAttachments('images/posts/post1');
+die('tset');
+$access_token = getFirstAccessKey($conn);
+
+$group_id_for_posting = $_GET['group_id_for_posting'];
+$group_id_for_get_post = $_GET['group_id_for_get_post'];
+$count = $_GET['count'];
+$offset = $_GET['offset'];
+$photos_in_post = $_GET['photos_in_post'];
+$comments = $_GET['comments'];
+$likes = $_GET['likes'];
+$reposts = $_GET['reposts'];
+$views = $_GET['views'];
+$count_text = $_GET['count_text'];
+$period_posts = $_GET['period_posts'];
+$start_time_for_post = $_GET['start_time_for_post'];
+
+$time_for_post = new DateTime($start_time_for_post, 'Europe/Kiev');
+
+$posting = new Posting($access_token);
+$posts = $posting->getFilteredPosts($group_id_for_get_post, $count, $offset,$photos_in_post,$comments,$likes,$reposts,$views,$count_text);
+
+
+if($posts) {
+    foreach ($posts as $post){
+        deleteSavedAttachments('images/posts/post1');
+        $post_text = getReplacedPostText($post['text']);
+        $post_attachments = getSavedAttachments($post['attachments']);
+        $posting->addPost($group_id_for_posting, $post_text, $post_attachments, $time_for_post->getTimestamp());
+        $time_for_post->add(new DateInterval('PT' . $period_posts . 'M'));
+    }
+    header("Location: /index.php");
+    $conn->close();
+} else{
+    die("За такими параметрами немає постів");
+    $conn->close();
+}
+
+function getSavedAttachments($post_attachments){
+    $imagePaths = [];
+    foreach ($post_attachments as $attachment){
+        if($attachment['type'] == 'photo'){
+            $max_width = 0;
+            $imageUrl = "";
+            foreach ($attachment['photo']['sizes'] as $size){
+                if($max_width < $size['width']){
+                    $imageUrl = $size['url'];
+                    $imageName = basename($imageUrl);
+                }
+            }
+            $imgPath = 'images/posts/post';
+            if (!file_exists($imgPath)) {
+                mkdir($imgPath, 0777, true);
+            }
+            $imgPath .= '/'.$imageName;
+            if(!file_put_contents($imgPath, file_get_contents($imageUrl))){
+                return false;
+            }else{
+                array_push($imagePaths, $imgPath);
+            }
+        }
+    }
+    return $imagePaths;
+}
+
+function deleteSavedAttachments($path){
+    if (file_exists($path)) {
+        rmdir($path);
+    }
+}
+
+function getReplacedPostText($post_text){
+    $letters = array('е' => 'e','а' => 'a', 'х' => 'x' ,'у' => 'y' , 'о' => 'o');
+    foreach ($letters as $russian_letter => $english_letter){
+        $post_text = str_replace($russian_letter, $english_letter, $post_text);
+    }
+    return $post_text;
+}
+
+
+function getFirstAccessKey($conn){
+    $sql = "SELECT access_tokens FROM Access_Tokens";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+    $access_token = $row['access_tokens'];
+    return $access_token;
+}
