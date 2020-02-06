a59bd31f1586ac1ec760ee239d4f11e17c198ad2
Viktor Dmytruk <bitbok@i.ua>
2020-02-06 18:59:46 +0200

    test date

--- post-to-vk.php
+++ post-to-vk.php
@@ -68,17 +68,19 @@ function getSavedAttachments($post_attachments){
 
 function deleteSavedAttachments(){
     $dir = 'images'. DIRECTORY_SEPARATOR . 'posts'. DIRECTORY_SEPARATOR .'post100';
-    $it = new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::SKIP_DOTS);
-    $files = new RecursiveIteratorIterator($it,
-        RecursiveIteratorIterator::CHILD_FIRST);
-    foreach($files as $file) {
-        if ($file->isDir()){
-            rmdir($file->getRealPath());
-        } else {
-            unlink($file->getRealPath());
+    if(is_dir($dir)){
+        $it = new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::SKIP_DOTS);
+        $files = new RecursiveIteratorIterator($it,
+            RecursiveIteratorIterator::CHILD_FIRST);
+        foreach($files as $file) {
+            if ($file->isDir()){
+                rmdir($file->getRealPath());
+            } else {
+                unlink($file->getRealPath());
+            }
         }
+        rmdir($dir);
     }
-    rmdir($dir);
 }
 
 function getReplacedPostText($post_text){
