a4286892b5bf64b989810ee78a16b6f215b443b1
Viktor Dmytruk <bitbok@i.ua>
2020-02-11 18:43:35 +0200

    form changes

--- index.php
+++ index.php
@@ -99,38 +99,38 @@ else {
 	<input type="text" name="group_id_for_posting" required value="<?php echo $group_id_for_posting?>">
 
 	<label for="group_id_for_get_post">Ід групи де брати пости</label>
-	<input type="text" name="group_id_for_get_post"  value="<?php echo $group_id_for_get_post?> required>
+	<input type="text" name="group_id_for_get_post"  value="<?php echo $group_id_for_get_post?>" required>
 
 	<label for="count">Кількість постів</label>
-	<input type="text" name="count" required  value="<?php echo $count?>>
+	<input type="text" name="count" required  value="<?php echo $count?>">
 
 	<label for="offset">Кількість пропусків постів</label>
-	<input type="text" name="offset" value="<?php echo $offset?> required >
+	<input type="text" name="offset" value="<?php echo $offset?>" required >
 
 	<label for="photos_in_post">Мінімальна кількість фото в пості</label>
-	<input type="text" name="photos_in_post"  value="<?php echo $photos_in_post?> required>
+	<input type="text" name="photos_in_post"  value="<?php echo $photos_in_post?>" required>
 
 	<label for="comments">Мінімальна кількість коментарів</label>
-	<input type="text" name="comments"  value="<?php echo $comments?> required>
+	<input type="text" name="comments"  value="<?php echo $comments?>" required>
 
 	<label for="likes">Мінімальна кількість лайків в пості</label>
-	<input type="text" name="likes"  value="<?php echo $likes?> required>
+	<input type="text" name="likes"  value="<?php echo $likes?>" required>
 
 	<label for="reposts">Мінімальна кількість репостів в пості</label>
-	<input type="text" name="reposts"  value="<?php echo $reposts?> required>
+	<input type="text" name="reposts"  value="<?php echo $reposts?>" required>
 
 	<label for="views">Мінімальна кількість охвата поста</label>
-	<input type="text" name="views"  value="<?php echo $views?> required>
+	<input type="text" name="views"  value="<?php echo $views?>" required>
 
 	<label for="cout_text">Мінімальна кількість символів в тексті</label>
-	<input type="text" name="cout_text"  value="<?php echo $count_text?> required>
+	<input type="text" name="cout_text"  value="<?php echo $count_text?>" required>
 
 
 	<label for="start_time_for_post">Дата від якої починати кидати в відложку Рік-місяць-день година:хвилина</label>
-	<input type="text" name="start_time_for_post" required  value="<?php echo $start_time_for_post?>>
+	<input type="text" name="start_time_for_post" required  value="<?php echo $start_time_for_post?>">
 
 	<label for="period_posts">Періодичність постів в (хв)</label>
-	<input type="text" name="period_posts" required  value="<?php echo $period_posts?>>
+	<input type="text" name="period_posts" required  value="<?php echo $period_posts?>">
 
 	<button type="submit">Добавити пости</button>
 </form>
