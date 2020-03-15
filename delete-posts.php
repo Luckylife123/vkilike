237b7f88eed434039ba7c6c1bae11c371bb019a1
Viktor Dmytruk <bitbok@i.ua>
2020-03-15 17:52:02 +0200

    Added delete posts

--- delete-posts.php
+++ delete-posts.php
@@ -31,7 +31,7 @@ if($posts) {
         }
     }
     foreach ($posts as $post => $key){
-        die($post['views']);
+        die($post);
         if($post_views_count = $post['views']['count']){
             unset($posts[$post]);
         }
