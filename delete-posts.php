5fdfd9a312802647d51c10f39534edef7cc954e9
Viktor Dmytruk <bitbok@i.ua>
2020-03-15 18:16:46 +0200

    Added delete posts

--- delete-posts.php
+++ delete-posts.php
@@ -13,7 +13,7 @@ $post_views_count = 0;
 
 if($posts) {
     foreach ($posts as $key => $post){
-        print_r($post);
+        print_r($key);
         die($posts);
         if($post_views_count > $post['views']['count']){
             $post_views_count = $post['views']['count'];
