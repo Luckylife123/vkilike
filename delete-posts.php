08a70b19c17419ae92c8ed5256c8e64a133ff6d2
Viktor Dmytruk <bitbok@i.ua>
2020-03-15 16:52:44 +0200

    Added delete posts

--- delete-posts.php
+++ delete-posts.php
@@ -10,8 +10,7 @@ $offset = $_GET['offset'];
 $posting = new Posting($access_token);
 $posts = $posting->getPosts($group_id_for_delete_posts, $count, $offset);
 $post_views_count = 0;
-print_r($posts);
-die("Tset");
+
 if($posts) {
     foreach ($posts as $key => $post){
         if($post_views_count > $post['views']['count']){
@@ -36,6 +35,8 @@ if($posts) {
             unset($posts[$key]);
         }
     }
+    print_r($posts);
+    die("Tset");
 
     foreach ($posts as $key => $post){
         $posting->deletePost($group_id_for_delete_posts, $post['id']);
